23aee2b652d0c66a18b4f9e2645024eb5e3db679
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-23 13:01:09 -0500

    speakers items load logic

---
+++ data.js
@@ -0,0 +1,38 @@
+export const speakers = [
+    {
+        name: "Juan Sanchez",
+        title: "React programmer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem architecto perspiciatis fugiat doloribus qui maiores voluptate voluptatum velit",
+        img: "./img/profile_photo_1.jpg"
+    },
+    {
+        name: "Carlos Gomez",
+        title: "React programmer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem architecto perspiciatis fugiat doloribus qui maiores voluptate voluptatum velit",
+        img: "./img/profile_photo_2.jpg"
+    },
+    {
+        name: "Nicole Rodriguez",
+        title: "React programmer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem architecto perspiciatis fugiat doloribus qui maiores voluptate voluptatum velit",
+        img: "./img/profile_photo_3.jpg"
+    },
+    {
+        name: "Elsa Chu",
+        title: "React programmer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem architecto perspiciatis fugiat doloribus qui maiores voluptate voluptatum velit",
+        img: "./img/profile_photo_4.jpg"
+    },
+    {
+        name: "Rene Wong",
+        title: "React programmer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem architecto perspiciatis fugiat doloribus qui maiores voluptate voluptatum velit",
+        img: "./img/profile_photo_5.jpg"
+    },
+    {
+        name: "Andrea Wise",
+        title: "React programmer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem architecto perspiciatis fugiat doloribus qui maiores voluptate voluptatum velit",
+        img: "./img/profile_photo_6.jpg"
+    },
+]
